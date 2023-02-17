0aecff2ebb748e32025d6b35f9853911cae9b678
‘OseiNana’ <osein2021@gmail.com>
2023-02-16 16:04:35 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Entry point a c program that prints a line to the standard e
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
